a585bca89709ceb76661d4d5bfdc27757398bdf4
Jonathon Cihlar <36781511+jonathoncihlar@users.noreply.github.com>
2024-11-11 14:30:19 -0700

    Update hello_you.py with solution

--- hello_you.py
+++ hello_you.py
@@ -1,12 +1,13 @@
 """
-
-
+This Python file contains the solution for 
+AP Computer Science Principles - Semester 1 - Practice 5
+Monarch High School, Boulder Valley School District
+J. Cihlar - 2024
 """
 
-
 def main():
-    pass
-
-
+    name:str = input("Hello, what is your name? ")
+    print(f"Hello, {name}! It is nice to meet you.")
+    
 if __name__ == "__main__":
     main()
